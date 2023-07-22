f6d7ad4467a1e0a079903610f0b00fb6d4a4684b
SHEGSZYDOO <segunbenson123@gmail.com>
2023-07-21 17:17:22 -0700

    web py file to be committed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+
 import requests
-import redis
-from functools import wraps
+import time
+import functools
 
-store = redis.Redis()
+# Decorator to handle caching and tracking access count
+def cache_with_count(expiration_time=10):
+    cache = {}
 
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(url):
+            now = time.time()
+            if url in cache and now - cache[url]['timestamp'] < expiration_time:
+                cache[url]['count'] += 1
+            else:
+                response = func(url)
+                cache[url] = {'response': response, 'timestamp': now, 'count': 1}
+            return cache[url]['response']
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        return wrapper
 
-        count_key = "count:" + url
-        html = method(url)
+    return decorator
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
-
-
-@count_url_access
+# Get the page content from a URL with caching and access count tracking
+@cache_with_count()
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    return ""
+
+# Test the implementation with a slow response simulator
+def main():
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
+    fast_url = "https://www.example.com"
+
+    print(get_page(slow_url))  # This will be slow due to the simulator
+    print(get_page(slow_url))  # This should be cached and fast
+    print(get_page(fast_url))  # This should be fetched again since it's a different URL
+    print(get_page(fast_url))  # This should be cached and fast
+
+if __name__ == "__main__":
+    main()
