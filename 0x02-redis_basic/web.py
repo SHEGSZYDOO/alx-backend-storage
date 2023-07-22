a54b88ea102bef74d55646e186ef4f4a5a18268c
SHEGSZYDOO <segunbenson123@gmail.com>
2023-07-21 17:29:39 -0700

    web py file to be committed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
-
+"""
+web cache and tracker
+"""
 import requests
-import time
-import functools
+import redis
+from functools import wraps
 
-# Decorator to handle caching and tracking access count
-def cache_with_count(expiration_time=10):
-    cache = {}
+store = redis.Redis()
 
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(url):
-            now = time.time()
-            if url in cache and now - cache[url]['timestamp'] < expiration_time:
-                cache[url]['count'] += 1
-            else:
-                response = func(url)
-                cache[url] = {'response': response, 'timestamp': now, 'count': 1}
-            return cache[url]['response']
 
-        return wrapper
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-    return decorator
+        count_key = "count:" + url
+        html = method(url)
 
-# Get the page content from a URL with caching and access count tracking
-@cache_with_count()
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return ""
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
+return ""
 
 # Test the implementation with a slow response simulator
 def main():
@@ -40,6 +45,11 @@ def main():
     print(get_page(slow_url))  # This should be cached and fast
     print(get_page(fast_url))  # This should be fetched again since it's a different URL
     print(get_page(fast_url))  # This should be cached and fast
+
+    # Wait for more than 10 seconds to simulate cache expiration
+    time.sleep(11)
+
+    print(get_page(slow_url))  # This should be fetched again after expiration
 
 if __name__ == "__main__":
     main()
